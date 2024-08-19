5b3eb049eaee1c5ddd00a408b96bc48bd86ab657
roma0323 <r911223oger@gmail.com>
2024-08-19 22:07:49 +0800

    feat:studentDashboard chart

--- Server/mock_interview/views/frontend_redesign_router.py
+++ Server/mock_interview/views/frontend_redesign_router.py
@@ -11,15 +11,64 @@ def admim_dashboard():
 
 @frontend_redesign_router.route('/studentDashboard')
 def student_dashboard():
-    return render_template('studentDashboard.html')
+    # Data to be sent to the template
+    overview_data = {
+        'average': 75,
+        'pr_value': 70,
+        'practice_count': 8
+    }
+
+    interview_records = [
+        {
+            'school': '國立中央大學',
+            'department': '企業管理學系',
+            'date': '8/17',
+            'score': 97
+        },
+        {
+            'school': '國立中央大學',
+            'department': '企業管理學系',
+            'date': '8/17',
+            'score': 97
+        }
+    ]
+
+    # Data for the charts
+    trend_chart_data = {
+        'labels': ['last_7', 'last_6', 'last_5', 'last_4', 'last_3', 'last_2', 'last_1'],
+        'datasets': {
+            'total_avg': [65, 59, 80, 81, 56, 55, 40],
+            'content_quality': [75, 49, 70, 91, 66, 45, 50],
+            'facial_expression': [85, 69, 60, 71, 76, 35, 60],
+            'eye_contact': [55, 79, 90, 61, 86, 25, 70]
+        }
+    }
+
+    score_overview_chart_data = {
+        'labels': ['總平均', '回答內容', '臉部表情', '眼睛視線'],
+        'personal_scores': [40, 35, 20, 40],
+        'overall_scores': [20, 30, 25, 15]
+    }
+
+    return render_template(
+        'studentDashboard.html',
+        overview_data=overview_data,
+        interview_records=interview_records,
+        trend_chart_data=trend_chart_data,
+        score_overview_chart_data=score_overview_chart_data
+    )
+
+
 
 @frontend_redesign_router.route('/interview_questioning')
 def interview_questioning():
     question_text = "在面對生成式 AI 的快速發展背景下，您認為為什麼資料分析仍為重要的學習方向，以及學習的優勢是什麼？"
-    image_left = "../static/images/left_image.jpg"
-    image_right = "../static/images/right_image.jpg"
+    image_left = "../static/images/bg_coffee.jpeg"
+    image_right = "../static/images/bg_coffee.jpeg"
     
     return render_template('interview_questioning.html', question_text=question_text, image_left=image_left, image_right=image_right)
+
+
 
 
 

6b9201cc661c69b7ce77a876987ce371552805f1
tofudoctor <87269360+tofudoctor@users.noreply.github.com>
2024-08-22 16:02:03 +0800

    feat: db renew voicetranscription

--- Server/mock_interview/models/firebase_func.py
+++ Server/mock_interview/models/firebase_func.py
@@ -343,22 +343,16 @@ def delQuestionHistory(history_id:str):
 
 # Voice_Transcriptions
 # 新增Emotion_Recognition
-def addVoiceTranscriptions(speech_speed:List[int], transcript:str, interview_id:str):
+def addVoiceTranscriptions(speech_speed:List[int], transcript:str, history_id:str):
 
     voice = db.collection('Voice_Transcriptions').document()
     timestamp = SERVER_TIMESTAMP
 
-    # blob = bucket.blob(interview_id + '/' + voice.id + '.wav')
-    # blob.upload_from_file(audio_file, content_type='audio/wav')
-    # blob.make_public()
-    # audio = blob.public_url
-
     voice.set({
         'transcription_id': voice.id,
-        # "audio_file": audio,
         "speech_speed": speech_speed, 
         "transcript": transcript,
-        "interview_id": interview_id,
+        "history_id": history_id,
         "timestamp": timestamp,
     })
 
@@ -366,27 +360,20 @@ def addVoiceTranscriptions(speech_speed:List[int], transcript:str, interview_id:
 def delVoiceTranscriptions(transcription_id:str):
     
     voice_ref = db.collection('Voice_Transcriptions').document(transcription_id)
-    voice_query = db.collection('Voice_Transcriptions').where('transcription_id', '==', transcription_id).stream()
-    
-    for interview in voice_query:
-        interview_id = interview.to_dict()['interview_id']
-    # blob = bucket.blob(interview_id + '/' + transcription_id + '.wav')
-    # blob.delete()
-    
     voice_ref.delete()
 
 
 
 # Questions
 # 新增Questions
-def addQuestions(question_department:str, question_school:str, history_id:str, question_schooldepartment:str, qusetion_text:str, user_id:str):
+def addQuestions(question_department:str, question_school:str, interview_id:str, question_schooldepartment:str, qusetion_text:str, user_id:str):
     
     question = db.collection('Questions').document()
     question_create_time = SERVER_TIMESTAMP
 
     question.set({
         'question_id': question.id,
-        "history_id": history_id,
+        "interview_id": interview_id,
         "question_create_time": question_create_time,
         "question_department": question_department, 
         "question_school": question_school,
